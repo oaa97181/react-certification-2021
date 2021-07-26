15a1641c2eeb335438bf32f187b2d193f0c0ebe6
oaa97181 <oaa97181@hotmail.com>
2021-07-25 20:10:44 -0500

    refactor some files

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,27 +1,18 @@
 import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
 
 function HomePage() {
-  const history = useHistory();
+  useHistory();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
-
+  const { authenticated } = useAuth();
   return (
     <section className="homepage" ref={sectionRef}>
       <h1>Homepage</h1>
       {authenticated ? (
-        <>
-          {/*TODO display profile pic*/}
-        </>
+        <>{/* TODO display profile pic */}</>
       ) : (
         // <Link to="/login">let me in →</Link>
         <p>Videos go here (:</p>
